f36a597625182c2b865626def38b835e798e8508
Brookie Guzder-Williams <bguzder-williams@wri.org>
2020-04-27 12:04:22 -0700

    remove _to_gcs naming. im-prof to utils

--- gcs_helpers/utils.py
+++ gcs_helpers/utils.py
@@ -1,6 +1,9 @@
 import json
 
 
+#
+# I/O
+#
 def read_json(path,*key_path):
     with open(path,'r') as file:
         jsn=json.load(file)
@@ -12,3 +15,31 @@ def read_json(path,*key_path):
 def write_json(obj,path):
     with open(path,'w') as file:
         jsn=json.dump(obj,file)
+
+
+
+#
+# IMAGE HELPERS
+#
+def image_profile(lon,lat,crs,resolution,im,driver=GTIFF_DRIVER):
+    count,height,width=im.shape
+    x,y=transform(Proj(init='epsg:4326'),Proj(init=crs),lon,lat)
+    x,y=int(round(x)),int(round(y))
+    xmin=x-int(width/2)
+    ymin=y-int(height/2)
+    profile={
+        'count': count,
+        'crs': CRS.from_string(crs),
+        'driver': GTIFF_DRIVER,
+        'dtype': im.dtype,
+        'height': height,
+        'nodata': None,
+        'transform': Affine(resolution,0,xmin,0,-resolution,ymin),
+        'width': width }
+    if driver==GTIFF_DRIVER:
+        profile.update({
+            'compress': 'lzw',
+            'interleave': 'pixel',
+            'tiled': False
+        })
+    return profile
